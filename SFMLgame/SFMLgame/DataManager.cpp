2f1a739f2380aed030d98b2b5d741ad74766f2d4
Rafal Paprota <paprota55@gmail.com>
2019-05-06 20:39:24 +0200

    Added outfit config loader

--- SFMLgame/SFMLgame/DataManager.cpp
+++ SFMLgame/SFMLgame/DataManager.cpp
@@ -23,3 +23,20 @@ void DataManager::mapFloorCfg(std::string & floorName, int & height, int & width
 		file >> floorName;
 	}
 }
+
+void DataManager::outfitCFG(sf::Texture & texture, sf::IntRect & rectangle, float & speed)
+{
+	std::fstream file;
+	std::string textureName;
+	file.open("outfits/outfitCFG.txt");
+	if (file.is_open())
+	{
+		file >> textureName;
+		texture.loadFromFile(textureName);
+		file >> rectangle.left;
+		file >> rectangle.top;
+		file >> rectangle.width;
+		file >> rectangle.height;
+		file >> speed;
+	}
+}
--- SFMLgame/SFMLgame/DataManager.h
+++ SFMLgame/SFMLgame/DataManager.h
@@ -1,11 +1,14 @@
 #pragma once
 #include<string>
 #include<fstream>
+#include"SFML/Graphics.hpp"
+
 class DataManager
 {
 public:
 	DataManager();
 	~DataManager();
 	void mapFloorCfg(std::string & floorName, int &height, int &width, int& size);
+	void outfitCFG(sf::Texture &, sf::IntRect &, float &);
 };
 
