ad1214641cfd5f3fafa71d8a8a4e461fbf0a5882
Rafal <paprota55@gmail.com>
2019-05-08 13:13:54 +0200

    Added object loader from file

--- SFMLgame/SFMLgame/DataManager.cpp
+++ SFMLgame/SFMLgame/DataManager.cpp
@@ -4,6 +4,9 @@
 
 DataManager::DataManager()
 {
+	objectsTxt = "object/objectCFG.txt";
+	mapNameTxt = "maps/floorCFG.txt";
+	outfitsTxt = "outfits/outfitCFG.txt";
 }
 
 
@@ -14,7 +17,7 @@ DataManager::~DataManager()
 void DataManager::mapFloorCfg(std::string & floorName, int & height, int & width, int &size)
 {
 	std::fstream file;
-	file.open("maps/floorCFG.txt");
+	file.open(mapNameTxt);
 	if (file.is_open())
 	{
 		file >> height;
@@ -22,13 +25,14 @@ void DataManager::mapFloorCfg(std::string & floorName, int & height, int & width
 		file >> size;
 		file >> floorName;
 	}
+	file.close();
 }
 
 void DataManager::outfitCFG(sf::Texture & texture, sf::IntRect & rectangle, float & speed)
 {
 	std::fstream file;
 	std::string textureName;
-	file.open("outfits/outfitCFG.txt");
+	file.open(outfitsTxt);
 	if (file.is_open())
 	{
 		file >> textureName;
@@ -39,4 +43,28 @@ void DataManager::outfitCFG(sf::Texture & texture, sf::IntRect & rectangle, floa
 		file >> rectangle.height;
 		file >> speed;
 	}
+	file.close();
+}
+
+void DataManager::loadObjects(std::vector<MapElement*>& objects)
+{
+	std::fstream file;
+	file.open(objectsTxt);
+	std::string textureName;
+	sf::IntRect objectRect;
+	int posX, posY, touchable;
+	if (file.is_open())
+	{
+		while (!file.eof())
+		{
+			file >> textureName;
+			file >> objectRect.left;
+			file >> objectRect.top;
+			file >> objectRect.width;
+			file >> objectRect.height;
+			file >> posX >> posY;
+			objects.push_back(new MapElement(textureName, objectRect, sf::Vector2f(posX, posY)));
+		}
+	}
+	file.close();
 }
--- SFMLgame/SFMLgame/DataManager.h
+++ SFMLgame/SFMLgame/DataManager.h
@@ -2,13 +2,19 @@
 #include<string>
 #include<fstream>
 #include"SFML/Graphics.hpp"
+#include "MapElement.h"
 
 class DataManager
 {
+	std::string mapNameTxt;
+	std::string touchableObjectsTxt;
+	std::string objectsTxt;
+	std::string outfitsTxt;
 public:
 	DataManager();
 	~DataManager();
 	void mapFloorCfg(std::string & floorName, int &height, int &width, int& size);
 	void outfitCFG(sf::Texture &, sf::IntRect &, float &);
+	void loadObjects(std::vector<MapElement*>&);
 };
 
