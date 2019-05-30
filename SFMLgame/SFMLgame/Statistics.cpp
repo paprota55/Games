c8205fc01933b361b25a04edce26d647d4ad746e
Rafal <paprota55@gmail.com>
2019-05-30 09:23:07 +0200

    Added variables and getters/setters

--- SFMLgame/SFMLgame/Statistics.cpp
+++ SFMLgame/SFMLgame/Statistics.cpp
@@ -6,7 +6,106 @@ Statistics::Statistics()
 {
 }
 
-
 Statistics::~Statistics()
 {
+}
+
+int Statistics::getHealth()
+{
+	return health;
+}
+
+int Statistics::getMana()
+{
+	return mana;
+}
+
+int Statistics::getExp()
+{
+	return experience;
+}
+
+int Statistics::getAttackSpeed()
+{
+	return attackSpeed;
+}
+
+int Statistics::getAttackDmg()
+{
+	return attackDmg;
+}
+
+int Statistics::getDefense()
+{
+	return defense;
+}
+
+int Statistics::getArmor()
+{
+	return armor;
+}
+
+int Statistics::getExpToLvl()
+{
+	return experienceToNextLevel;
+}
+
+int Statistics::getCurrHp()
+{
+	return currHp;
+}
+
+int Statistics::getCurrMp()
+{
+	return currMp;
+}
+
+void Statistics::setHealth(int & tmp)
+{
+	health = tmp;
+}
+
+void Statistics::setMana(int & tmp)
+{
+	mana = tmp;
+}
+
+void Statistics::setExp(int & tmp)
+{
+	experience = tmp;
+}
+
+void Statistics::setAttackSpeed(int & tmp)
+{
+	attackSpeed = tmp;
+}
+
+void Statistics::setAttackDmg(int & tmp)
+{
+	attackDmg = tmp;
+}
+
+void Statistics::setDefense(int & tmp)
+{
+	defense = tmp;
+}
+
+void Statistics::setArmor(int & tmp)
+{
+	armor = tmp;
+}
+
+void Statistics::setExpToLvl(int & tmp)
+{
+	experienceToNextLevel = tmp;
+}
+
+void Statistics::setCurrHp(int & tmp)
+{
+	currHp = tmp;
+}
+
+void Statistics::setCurrMp(int & tmp)
+{
+	currMp = tmp;
 }
--- SFMLgame/SFMLgame/Statistics.h
+++ SFMLgame/SFMLgame/Statistics.h
@@ -1,10 +1,29 @@
 #pragma once
 class Statistics
 {
-	int health, mana,experience,sword_skill,distance_skill,glove_skill,defense,armor;
-
+	int health, currHp, mana,currMp,experience, experienceToNextLevel, attackSpeed, attackDmg,defense,armor;
 public:
 	Statistics();
 	~Statistics();
-};
+	int getHealth();
+	int getMana();
+	int getExp();
+	int getAttackSpeed();
+	int getAttackDmg();
+	int getDefense();
+	int getArmor();
+	int getExpToLvl();
+	int getCurrHp();
+	int getCurrMp();
 
+	void setHealth(int &tmp);
+	void setMana(int &tmp);
+	void setExp(int &tmp);
+	void setAttackSpeed(int &tmp);
+	void setAttackDmg(int &tmp);
+	void setDefense(int &tmp);
+	void setArmor(int &tmp);
+	void setExpToLvl(int &tmp);
+	void setCurrHp(int &tmp);
+	void setCurrMp(int &tmp);
+};
